f1a4d42a15f476e67ba5da6d7d6620172f35f89b
marco villanueva <villanuevamarco98@gmail.com>
2020-08-03 20:31:21 -0600

    Add project files.

---
+++ LearningExceptionFilters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LearningExceptionFilters
+{
+    class PersonException : Exception
+    {
+        public PersonException(String name)
+        {
+            Name = name;
+        }
+        public String Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //throw new PersonException("Kim");
+                throw new PersonException("Chap");
+            }
+            catch (PersonException ex)
+            {
+                if (ex.Name == "Kim")
+                    Console.WriteLine("Hang up on them!");
+                if(ex.Name == "Chap")
+                    Console.WriteLine("It's chap!");
+
+            }
+        }
+    }
+}
